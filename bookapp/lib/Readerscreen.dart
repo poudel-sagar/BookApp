e026d120e47e5d8e756991e6e987a73d587a4236
poudel-sagar <sagarpoudel2020@gmail.com>
2022-03-04 13:11:26 +0545

    commit

---
+++ bookapp/lib/Readerscreen.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ReaderScreen extends StatefulWidget {
+
+final int chapterID;
+ReaderScreen(this.chapterID);
+  @override
+  State<ReaderScreen> createState() => _ReaderScreenState();
+}
+WebViewController _controller;
+class _ReaderScreenState extends State<ReaderScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chapter'+widget.chapterID.toString()),
+      ),
+      body: WebView(
+        initialUrl: 'about:blank',
+        onWebViewCreated: (WebViewController webviewcontroller){
+          _controller = webviewcontroller;
+          _loadHtmlCh1();
+        },
+      ),
+      
+    );
+  }
+}
+
+_loadHtmlCh1() async{
+  String fileText = await rootBundle.loadString("assets/ch1.html");
+  _controller.loadUrl(Uri.dataFromString(
+    fileText,mimeType: 'text.html',
+  encoding: Encoding.getByName('utf-8')).toString());
+}
+
+_loadHtmlCh2() async{
+  String fileText = await rootBundle.loadString("assets/ch2.html");
+  _controller.loadUrl(Uri.dataFromString(
+    fileText,mimeType: 'text.html',
+  encoding: Encoding.getByName('utf-8')).toString());
+}
---
+++ bookapp/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:bookapp/Readerscreen.dart';
+import 'package:flutter/material.dart';
+void main()=>runApp(MaterialApp(
+  title: "book app",
+  debugShowCheckedModeBanner: false,
+  home: HomePage(),
+)
+);
+
+var chapterLists=[
+  'ch 1',
+  'ch 2',
+  'ch 3',
+  'ch 4',
+  'ch 5',
+
+];
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Book App"),
+        backgroundColor: Color(0xFFB4161B),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: chapterLists.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 2.0,
+            mainAxisSpacing: 2.0
+          ),
+           itemBuilder: (BuildContext context, int index){
+             return GestureDetector(
+               onTap: (){
+                 if(chapterLists[index]=="ch 1"){
+                   Navigator.push(
+                     context,
+                     MaterialPageRoute(builder: (context)=> ReaderScreen(1)),
+                   );
+
+                 }else if(chapterLists[index]=="ch 2"){
+                    Navigator.push(
+                     context,
+                     MaterialPageRoute(builder: (context)=> ReaderScreen(2)),
+                   );
+                  
+                 }
+               },
+               child: Card(
+                 child: Column(
+                   children: [
+                     SizedBox(height: 5),
+                     Icon(Icons.book, size: 40),
+                     SizedBox(height: 5),
+                     Text(chapterLists[index], style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+                   ],
+
+                 ),
+               ),
+             );
+           }),
+      )
+      
+    );
+  }
+}
